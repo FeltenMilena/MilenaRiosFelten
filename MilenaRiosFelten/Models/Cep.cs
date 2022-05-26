b291bc4650104f12b2418f9aa9db307c60534332
Milena Rios Felten <milena.felten@unidasul.com.br>
2022-05-26 18:22:30 -0300

    Add project files.

---
+++ MilenaRiosFelten/Models/Cep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MilenaRiosFelten.Models
+{
+    public class Cep
+    {
+        public string Codigo { get; set; }
+    }
+}
---
+++ MilenaRiosFelten/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MilenaRiosFelten.Models
+{
+    public class Endereco
+    {
+        public string UF { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+    }
+}
